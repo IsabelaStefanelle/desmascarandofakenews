4f70fd1e3f0edbd87ae146f5cf170b89c9d55ac4
Ruan2009-off <ruan.fagundes@etec.sp.gov.br>
2025-12-02 22:21:02 -0300

    craiação do projeto

---
+++ src/InterfaceDoUsuario/TelaDeBusca.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Desmascarandofakenews\InterfaceDoUsuario;
+
+class TelaDeBusca{
+
+    public string $campo_de_Pesquisa ;
+
+    public function enviarpesquisa():void {
+
+    }
+}
---
+++ tests/SistemaExterno/APITest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace test\SistemaExterno;
+
+use PHPUnit\Framework\TestCase;
+use Desmascarandofakenews\SistemaExterno\API;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(API::class)]
+
+
+class APITest extends TestCase
+{    
+    public function testCriacaoDeObjetos(){        
+        $API = new API();
+
+        $this->assertInstanceOf(API::class,$API);
+        
+        $API->urlEndpoint = 'https://api.exemplo.com/v1/noticias';
+        $API->chaveAPI = 'abc12345-fake-key-987zyx';
+        
+        $this->assertEquals('https://api.exemplo.com/v1/noticias',$API->urlEndpoint);
+        $this->assertEquals('abc12345-fake-key-987zyx', $API->chaveAPI);
+
+    }
+}
